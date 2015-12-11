392936dd551bde41a84382db2d691e4357369059
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-11 15:40:39 -0500

    Add angular and jquery

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const express  = require('express');
 const app      = express();
 const logger   = require('morgan');
 
-const placesRoutes = require('./routes/places.js');
+const placesRoutes  = require('./routes/places.js');
 const weatherRoutes = require('./routes/weather.js');
 
 app.use('/places', placesRoutes);
 app.use('/weather', weatherRoutes);
+app.use('/scripts', express.static(__dirname + '/node_modules/angular'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.send({ SUCCESS: true });
