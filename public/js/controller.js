1178d56f78f2f7395e532529b267604377da9384
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-11 16:08:25 -0500

    Add google maps library into index.html

--- public/js/controller.js
+++ public/js/controller.js
@@ -5,6 +5,7 @@ let app = angular.module('Vicinity', []);
 app.controller('VicinityController', function(){
   this.test = function() {
     console.log('testing...button clicked');
+    initialize();
   };
 
 
@@ -12,4 +13,17 @@ app.controller('VicinityController', function(){
 
 $(function() {
   console.log('controller.js loaded');
+
 });
+
+function initialize() {
+  let center = new google.maps.LatLng(51.508742, -0.120850);
+
+  let mapProp = {
+    center:    center,
+    zoom:      10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  let map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
+}
