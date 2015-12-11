106ebc2de0dadd57abd7924d100e110872e8473c
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-11 15:46:50 -0500

    Set up angular and connected the controller to index.html

--- public/js/controller.js
+++ public/js/controller.js
@@ -1,3 +1,15 @@
+'use strict'
+
+let app = angular.module('Vicinity', []);
+
+app.controller('VicinityController', function(){
+  this.test = function() {
+    console.log('testing...button clicked');
+  };
+
+
+});
+
 $(function() {
   console.log('controller.js loaded');
 });
