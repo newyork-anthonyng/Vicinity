690fadef0a9f21f803e38db65fa2b4a639315cd5
General Assembly | Anthony Ng <newyork.anthonyng@gmail.com>
2015-12-12 11:29:28 -0500

    Add button to retrieve current location and show on google map

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,28 +2,49 @@
 
 let app = angular.module('Vicinity', []);
 
-app.controller('VicinityController', function(){
-  this.test = function() {
-    console.log('testing...button clicked');
-    initialize();
+app.controller('VicinityController', function() {
+  this.currentLatitude = -1;
+  this.currentLongitude = -1;
+
+  this.categories = ['bar', 'cafe', 'casino', 'convenience_store',
+                     'liquor_store', 'museum', 'park', 'restaurant',
+                     'shopping_mall', 'points_of_interest'];
+
+  // use browser to get current location
+  this.getCurrentLocation = function() {
+    console.log(this);
+    console.log(this.currentLatitude, this.currentLongitude);
+
+    if(navigator.geolocation) {
+      // use .bind(this) to be able to access Controller variables
+      navigator.geolocation.getCurrentPosition(this.displayPositionOnMap.bind(this));
+    } else {
+      console.log('Geolocation is not supported by this browser. \
+      Please make sure to enable location.');
+    }
   };
 
+  // display location onto map
+  this.displayPositionOnMap = function(position) {
+    // get coordinates and save into controller
+    let myLatitude  = position.coords.latitude;
+    let myLongitude = position.coords.longitude;
+    this.currentLatitude  = myLatitude;
+    this.currentLongitude = myLongitude;
 
-});
+    // create map and with marker on current location
+    let center = new google.maps.LatLng(myLatitude, myLongitude);
 
-$(function() {
-  console.log('controller.js loaded');
+    let mapProp = {
+      center:    center,
+      zoom:      16,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-});
-
-function initialize() {
-  let center = new google.maps.LatLng(51.508742, -0.120850);
-
-  let mapProp = {
-    center:    center,
-    zoom:      10,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    let myMap  = document.getElementById('current-location-map');
+    let map    = new google.maps.Map(myMap, mapProp);
+    let marker = new google.maps.Marker({ position: center });
+    marker.setMap(map);
   };
 
-  let map = new google.maps.Map(document.getElementById('googleMap'), mapProp);
-}
+}); // end of VicinityController
